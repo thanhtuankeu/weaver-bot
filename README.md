286d7a9cc8e5f6b66b98ea38c1ca66ba6270e29a
Glitch (tuannghiem) <none>
2020-09-25 14:37:20 +0000

    🐟👂 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# weaver-bot
+# Flask resume template 
+
+A simple Flask resume template based on <https://startbootstrap.com/themes/resume/>.
--- src/app/views/plugins/spinners/spinners.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  templateUrl: 'spinners.component.html',
-  styleUrls: ['./spinners.scss'],
-  encapsulation: ViewEncapsulation.None
-})
-export class SpinnersComponent { }
